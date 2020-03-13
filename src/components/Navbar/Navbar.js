7f9b40c6c04b2654b7a1c330bb9db826b44f101f
MohitDhungana <mohitdhungana@yahoo.com>
2020-03-13 16:28:50 +0545

    initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './navbar.scss';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="city tours" srcset="" />
+      <ul className="nav-link">
+        <li>
+          <a href="/" className="nav-links">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-links">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-links active">
+            Tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
